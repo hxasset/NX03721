521305279a36d508eab48233ca97a8ab2ed6d317
hxasset <hxasset0307@gmail.com>
2025-06-24 10:44:45 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,439 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-l",
+      "name": "L",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.1411283861837838,
+        "pitch": 0.0316045905139255,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.949730811131392,
+          "pitch": -0.02994813771061189,
+          "rotation": 1.5707963267948966,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.311103751688166,
+          "pitch": -0.417386988962825,
+          "title": "匯幸資產版權所有",
+          "text": "<span class=\"NormalTextRun SCXW106598891 BCX0\" style=\"-webkit-user-drag: none; -webkit-tap-highlight-color: transparent; margin: 0px; padding: 0px; user-select: text; color: rgb(0, 0, 0); font-family: &quot;Microsoft JhengHei&quot;, &quot;Microsoft JhengHei_EmbeddedFont&quot;, &quot;Microsoft JhengHei_MSFontService&quot;, sans-serif; font-size: 14.6667px; font-variant-ligatures: none; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">&lt;</span><span class=\"NormalTextRun SCXW106598891 BCX0\" style=\"-webkit-user-drag: none; -webkit-tap-highlight-color: transparent; margin: 0px; padding: 0px; user-select: text; color: rgb(0, 0, 0); font-family: &quot;Microsoft JhengHei&quot;, &quot;Microsoft JhengHei_EmbeddedFont&quot;, &quot;Microsoft JhengHei_MSFontService&quot;, sans-serif; font-size: 14.6667px; font-variant-ligatures: none; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">202506</span><span class=\"NormalTextRun SCXW106598891 BCX0\" style=\"-webkit-user-drag: none; -webkit-tap-highlight-color: transparent; margin: 0px; padding: 0px; user-select: text; color: rgb(0, 0, 0); font-family: &quot;Microsoft JhengHei&quot;, &quot;Microsoft JhengHei_EmbeddedFont&quot;, &quot;Microsoft JhengHei_MSFontService&quot;, sans-serif; font-size: 14.6667px; font-variant-ligatures: none; white-space-collapse: preserve; background-color: rgb(255, 255, 255);\">&gt;</span>"
+        },
+        {
+          "yaw": 2.3072463923342115,
+          "pitch": -0.2657074256584053,
+          "title": "<img src='floorplan10.jpg'>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.408231836867209,
+        "pitch": 0.2744786895990803,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3966269027155551,
+          "pitch": 0.0824544577252464,
+          "rotation": 0,
+          "target": "2-2"
+        },
+        {
+          "yaw": 2.6753790425705954,
+          "pitch": 0.07721288559109318,
+          "rotation": 4.71238898038469,
+          "target": "0-l"
+        },
+        {
+          "yaw": 2.9382966636765744,
+          "pitch": 0.002954099020406886,
+          "rotation": 0,
+          "target": "4-4"
+        },
+        {
+          "yaw": 2.809715493424876,
+          "pitch": 0.008350919369778609,
+          "rotation": 4.71238898038469,
+          "target": "3-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.430101417705547,
+        "pitch": -0.02180337964611745,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4859845690066429,
+          "pitch": 0.010571078953923063,
+          "rotation": 0,
+          "target": "1-1"
+        },
+        {
+          "yaw": -2.891578195594006,
+          "pitch": -0.06808677717140377,
+          "rotation": 7.0685834705770345,
+          "target": "7-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.80498626250861,
+        "pitch": 0.052883101301059554,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8460615058704235,
+          "pitch": -0.038147761854098405,
+          "rotation": 0,
+          "target": "6-6"
+        },
+        {
+          "yaw": -1.363772300293606,
+          "pitch": -0.04772075561155731,
+          "rotation": 1.5707963267948966,
+          "target": "5-5"
+        },
+        {
+          "yaw": -0.4103010410134651,
+          "pitch": -0.0003746361057608283,
+          "rotation": 4.71238898038469,
+          "target": "4-4"
+        },
+        {
+          "yaw": 0.11057876791889143,
+          "pitch": -0.00033239873688195587,
+          "rotation": 1.5707963267948966,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.03205706789377416,
+        "pitch": -0.17688021142829413,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1227009752285326,
+          "pitch": 0.08007936137085458,
+          "rotation": 0,
+          "target": "1-1"
+        },
+        {
+          "yaw": -2.7439971691960547,
+          "pitch": -0.0017114283429009447,
+          "rotation": 1.5707963267948966,
+          "target": "0-l"
+        },
+        {
+          "yaw": -2.49906067122628,
+          "pitch": 0.0008249611023671832,
+          "rotation": 1.5707963267948966,
+          "target": "3-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.9563691921642388,
+        "pitch": 0.11388177996068904,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.051071586485488,
+          "pitch": 0.08094062177136507,
+          "rotation": 1.5707963267948966,
+          "target": "6-6"
+        },
+        {
+          "yaw": 3.0601760517731478,
+          "pitch": 0.08502378993290094,
+          "rotation": 4.71238898038469,
+          "target": "3-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-6",
+      "name": "6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.34728490218255104,
+        "pitch": 0.002423016594901739,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9802551002734674,
+          "pitch": 0.007329963802177986,
+          "rotation": 0,
+          "target": "3-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-7",
+      "name": "7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.12466637514244816,
+        "pitch": 0.07269049784723869,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.736301224038698,
+          "pitch": 0.31851994955334106,
+          "rotation": 3.141592653589793,
+          "target": "2-2"
+        },
+        {
+          "yaw": -0.6380625859458267,
+          "pitch": -0.006713985084257246,
+          "rotation": 4.71238898038469,
+          "target": "8-8"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4281741343734673,
+          "pitch": -0.5247790530600902,
+          "title": "格局圖",
+          "text": "<img src='floorplan11.jpg'>"
+        }
+      ]
+    },
+    {
+      "id": "8-8",
+      "name": "8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.805889197104027,
+        "pitch": 0.24230165949080806,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.665214261742218,
+          "pitch": -0.012255199120707161,
+          "rotation": 1.5707963267948966,
+          "target": "7-7"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
